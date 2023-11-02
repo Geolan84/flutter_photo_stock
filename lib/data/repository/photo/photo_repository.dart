77a7506bd466153a6c7b00bdeae826f83ca380a4
Lanin George <gmlanin@edu.hse.ru>
2023-11-03 02:02:18 +0300

    fix: remove old mock method for getting photos

--- lib/data/repository/photo/photo_repository.dart
+++ lib/data/repository/photo/photo_repository.dart
@@ -11,22 +11,4 @@ class PhotoRepository {
     Logger.d(res.toString());
     return res;
   }
-
-  Future<Iterable<Photo>> getAllPhotos() async {
-    //Imitation of work.
-    await Future.delayed(const Duration(seconds: 1));
-    return Future(
-      () => List<Photo>.generate(
-        10,
-        (index) => Photo(
-          imageLink:
-              'https://wallpapers.com/images/hd/funny-shrek-h7n6n0iogb00kcan.jpg',
-          username: 'Shrek${index + 1}',
-          likes: 28,
-          color: '#260c0c',
-          blurHash: 'L6PZfSi_.AyE_3t7t7R**0o#DgR4',
-        ),
-      ),
-    );
-  }
 }
